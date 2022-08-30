9e44ad297cd7c8c08d02ffcc318de07f642c120c
hulkdx <hulktroll@gmail.com>
2022-08-30 15:42:20 +0300

    Hello world 3

--- 1_helloworld/hello_test.go
+++ 1_helloworld/hello_test.go
@@ -4,14 +4,20 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assert(t, got, want)
 	})
 
 	t.Run("hello to empty string", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assert(t, got, want)
+	})
+
+	t.Run("hello spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
 		assert(t, got, want)
 	})
 }
