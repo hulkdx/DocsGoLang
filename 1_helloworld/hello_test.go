363e02911dd007ab1c25af26557edefeb9391c7c
hulkdx <hulktroll@gmail.com>
2022-08-30 15:28:31 +0300

    Hello Empty World

--- 1_helloworld/hello_test.go
+++ 1_helloworld/hello_test.go
@@ -3,9 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assert(t, got, want)
+	})
 
+	t.Run("hello to empty string", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assert(t, got, want)
+	})
+}
+
+func assert(t *testing.T, got string, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
