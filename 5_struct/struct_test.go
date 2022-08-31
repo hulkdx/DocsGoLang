cb5beb59a5b8ffaffbb5566ca456e10179d97741
hulkdx <hulktroll@gmail.com>
2022-08-31 19:17:01 +0300

    5. struct

---
+++ 5_struct/struct_test.go
@@ -0,0 +1,21 @@
+package main
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.0
+	if got != want {
+		t.Errorf("got %f want %f given", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Area(rectangle)
+	want := 100.0
+	if got != want {
+		t.Errorf("got %f want %f given", got, want)
+	}
+}
