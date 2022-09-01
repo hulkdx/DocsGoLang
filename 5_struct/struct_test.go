12cfa2b36a631d7ed4c0fc884e80fdbd8448379e
hulkdx <hulktroll@gmail.com>
2022-09-01 13:25:18 +0300

    5. interfaces

--- 5_struct/struct_test.go
+++ 5_struct/struct_test.go
@@ -12,10 +12,38 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Area(rectangle)
-	want := 100.0
-	if got != want {
-		t.Errorf("got %f want %f given", got, want)
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		checkArea(t, rectangle, 72.0)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
+	})
+}
+
+func TestTableDrivenArea(t *testing.T) {
+	testCases := []struct {
+		shape Shape
+		want  float64
+	}{
+		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{shape: Circle{Radius: 10}, want: 314.1592653589793},
+	}
+	for _, tC := range testCases {
+		got := tC.shape.Area()
+		want := tC.want
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
 	}
 }
--- README.md
+++ README.md
@@ -18,6 +18,33 @@ checkSums := func(t testing.TB, got, want []int) {}
 checkSums(t, got, want)
 ```
 
+## Functions: for struct
+```go
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+func (r Rectangle) Area() float64 {
+	return (r.Width * r.Height)
+}
+```
+
+# Interfaces
+
+No need to inherit / implements, it just works.
+
+```go
+type Shape interface {
+	Area() float64
+}
+type Circle struct {
+	Radius float64
+}
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+```
+
 # godocs
 - install
 ```sh
